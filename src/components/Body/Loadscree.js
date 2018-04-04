f794925d70c7d2aad70b4fb7d207863927ec008e
jormin <jorminxee@gmail.com>
2018-04-04 10:25:23 +0800

    引入ant-design-pro

--- src/components/Body/Loadscree.js
+++ src/components/Body/Loadscree.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Lazyimg from 'react-lazyimg-component';
+import Ellipsis from 'ant-design-pro/lib/Ellipsis';
+import { Icon } from 'antd';
 import 'velocity-animate';
 import 'velocity-animate/velocity.ui';
 import placeholder from '../../img/loading2.gif';
@@ -13,7 +15,7 @@ class Loadscree extends Component {
                         <Lazyimg js_effect='transition.fadeIn' className="lazy am_img animated" src={loadscree.thumb} placeholder={placeholder} onClick={() => this.props.handleShowModal(this)}></Lazyimg>
                     </div>
                     <div className="am_listimg_info">
-                        <span className="am-icon-heart"> {loadscree.name}</span>
+                        <Ellipsis lines={1} tooltip><Icon type="heart"/> {loadscree.name}</Ellipsis>
                     </div>
                 </div>
             </li>
