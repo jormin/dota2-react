1ec93e787d01910dde1da3cd59d358c495822563
jormin <jorminxee@gmail.com>
2018-04-06 14:07:51 +0800

    修改404页面

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route } from 'react-router-dom'
+import { HashRouter, Switch, Route } from 'react-router-dom'
 import './css/app.css';
 import './css/amazeui.min.css';
 import './js/app.js';
@@ -13,12 +13,12 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <HashRouter history={this.history}>
-        <div>
+        <Switch>
             <Route exact path='/' component={Index}/>
             <Route exact path='/picture' component={Picture}/>
             <Route exact path='/video' component={Video}/>
             <Route component={NotFound} />
-        </div>
+        </Switch>
     </HashRouter>
     , document.getElementById('root'));
 registerServiceWorker();
